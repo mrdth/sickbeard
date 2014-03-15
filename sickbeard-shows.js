3014d13cf2617e5a0d1ff5351115e9019eb284bf
mrdth <mrdth@zeventien.org>
2014-03-15 01:57:29 +0000

    Added 'self' & 'stats' methods to the SickbeardShows class.

--- sickbeard-shows.js
+++ sickbeard-shows.js
@@ -1,9 +1,45 @@
 (function() {
-  var Q = require('q');
+  var Q  = require('q');
+  var QS = require('querystring');
 
   // Sickbeard shows class
   var SickbeardShows = function(delegate) {
     this.delegate = delegate;
+  };
+
+  /**
+   * Call the 'shows' method of the API, and return the data
+   * from the call.
+   * 
+   * @param object args
+   *   Optional arguments to be passed with the call.
+   *   
+   * @return object
+   *   JSON object describing the shows in the Sickbeard list.
+   *
+   * @see http://sickbeard.com/api/#shows
+   */
+  SickbeardShows.prototype.self = function(args) {
+    var delegate  = this.delegate;
+
+    return delegate.cmd('shows', args).then(function(response){
+      return response.data;
+    });
+  };
+
+  /**
+   * Call the 'stats' method of the API, and return the data
+   * from the call.
+   * 
+   * @return object
+   *   JSON object describing global episode and show statistics.
+   */
+  SickbeardShows.prototype.stats = function() {
+    var delegate  = this.delegate;
+
+    return delegate.cmd('shows.stats').then(function(response){
+      return response.data;
+    });
   };
 
   module.exports = SickbeardShows;
